cdd094edecaa8db164fc0381c2226c660f35baee
Kingoftheace <kingtheaceof@gmail.com>
2024-01-26 19:01:57 +0700

    Added update and delete functions to active_pairs

--- dbUpload.py
+++ dbUpload.py
@@ -13,16 +13,26 @@ class Upload:
         self.tables_info = Tables_info()
         self.remote_server = remote_server
         self.gecko = Gecko(logger=logger)
+        self.dicActivePairs = {}
+
+        self.dateNow = datetime.now(timezone.utc).date()
+        self.timeNow = datetime.now(timezone.utc).time().strftime("%H:%M:%S")
 
         self.logger = logger
 
-    def funding_rates(self, exchange_count: int = 40, min_fr: float = 0.015, min_vol: float = 15000, min_oi: float = 15000):
 
+
+    def get_funding_rate_data(self,exchange_count, min_fr: float = 0.015, min_vol: float = 15000, min_oi: float = 15000):
         self.logger.add('Data upload', 'Initialize', 'Success', 'Created Tables_info and gecko objects')
 
-        # ---------------------------------------- SETUP
-        # ----- Connect to Database and Get Table info
-        table_name = 'funding_rates_2h'
+        self.active_pairs(exchange_count, min_fr,min_vol,min_oi)
+        #self.funding_rates_2h()
+
+
+    def active_pairs(self,exchange_count: int = 50, min_fr: float = 0.015, min_vol: float = 15000, min_oi: float = 15000):
+
+        # ----- Connect to Database
+        table_name = 'active_pairs'
 
         try:
             connection = Connection(remote_server=self.remote_server)
@@ -37,45 +47,7 @@ class Upload:
             error_message = str(e)[-255:]
             self.logger.add('Data upload', 'Establishing connection to database', 'Error', error_message)
             self.logger.exit_code_run_due_to_error(error_message)
-
-        dicTableInfo = connection.get_table_info(table_name)
         # -----/
-
-        # ----- Check Time difference between last upload and now
-        try:
-            dateNow = datetime.now(timezone.utc).date()
-            timeNow = datetime.now(timezone.utc).time().strftime("%H:%M:%S")
-            fundingCycle = self.get_funding_cycle(refTime=datetime.now(timezone.utc).time(),is2hourCycle=True)
-
-            lastUpdDate = dicTableInfo['upload_last_date']
-            lastUpdTime = dicTableInfo['upload_last_time']
-            if lastUpdDate is not None:
-                lastFundingCycle = self.get_funding_cycle(refTime=lastUpdTime, is2hourCycle=True)
-            # -----/
-
-            self.logger.add('Data upload', 'Calculating funding Cycle', 'Success', 'Calculated')
-        except Exception as e:
-            error_message = str(e)[-255:]
-            self.logger.add('Data upload', 'Calculating funding Cycle', 'Error', f'{error_message}')
-            self.logger.exit_code_run_due_to_error(f'{error_message}')
-
-
-        # ----- In case we have data for the same funding cycle, delete the previous data and re-upload
-        try:
-            if lastUpdDate is not None and fundingCycle == lastFundingCycle and lastUpdDate == dateNow:
-                connection.delete_date_time_from_table(table_name=table_name,dateCol='utc_date',timeCol='utc_time',params=(lastUpdDate,lastUpdTime))
-                self.logger.add('Data upload', 'Delete previous data for the day and Cycle', 'Success', 'Deleted')
-        except Exception as e:
-            error_message = str(e)[-255:]
-            self.logger.add('Data upload', 'Delete previous data for the day and Cycle', 'Error', f'{error_message}')
-            self.logger.exit_code_run_due_to_error(f'{error_message}')
-        # -----/
-
-        # ----- Get column Names
-        fr_columns = list(self.tables_info.funding_rates().keys())      # Funding Rate columns
-        ul_columns = list(self.tables_info.update_log().keys())         # Upload log columns
-        # -----/
-        # ----------------------------------------/
 
         # ---------------------------------------- GET DATA FROM API
         self.gecko.get_exchanges(exchange_count)
@@ -85,98 +57,30 @@ class Upload:
                         f'Ready to upload the data to DB')
         # ----------------------------------------/
 
-        # Get active pairs
-        dicActivePairs = connection.get_uniq_values_from_col('active_pairs','exchange_id_symbol')
+        # ----- Get active pairs
+        self.dicActivePairs = connection.get_uniq_values_from_col('active_pairs','exchange_id_symbol')
         dicToActivePairs = {}   # New pairs to be added
+        stillActive = []        # IDs that still passed the filters
+        # -----/
 
-
-        # ---------------------------------------- FUNDING RATE DATA
-        try:
-            dicExchanges = {}           # Key: exchange_id, Value: dic with currency- and pairs counts
-            data = []
-            for ep in self.gecko.exchange_pairs.values():
-
-                # ----- Either our FR + Vol meets criteria this Cycle, or it met criteria in the past (is in active_pairs)
-                exchange_symbol = f'{ep.exchange_id}_{ep.symbol}'
-                criteriaPass = False
-                if abs(ep.funding_rate) >= min_fr and ep.volume_usd >= min_vol and ep.open_interest >= min_oi:
-                    criteriaPass = True
-                    if not exchange_symbol in dicActivePairs:
-                        dicToActivePairs[exchange_symbol] = ep
+        # ----- Get new pairs that meet our criteria
+        for ep in self.gecko.exchange_pairs.values():
+            exchange_symbol = f'{ep.exchange_id}_{ep.symbol}'
+            if abs(ep.funding_rate) >= min_fr and ep.volume_usd >= min_vol and ep.open_interest >= min_oi:
+                if not exchange_symbol in self.dicActivePairs:
+                    self.dicActivePairs[exchange_symbol] = 1
+                    dicToActivePairs[exchange_symbol] = ep
                 else:
-                    if exchange_symbol in dicActivePairs:
-                        criteriaPass = True
-                # -----/
-
-
-                # ----- Update log data (for each exchange)
-                if ep.exchange_id not in dicExchanges:
-                    dicExchanges[ep.exchange_id] = {}
-                    dicExchanges[ep.exchange_id]['currencies_count'] = 0
-                    dicExchanges[ep.exchange_id]['pairs_count'] = 0
-                    dicExchanges[ep.exchange_id]['volume'] = 0
-                    ul_data_row = ()
-                    currencies = []  # Currency list for the active exchange
-
-                if ep.base not in currencies:
-                    dicExchanges[ep.exchange_id]['currencies_count'] +=1            # Currencies count
-                    currencies.append(ep.base)
-                dicExchanges[ep.exchange_id]['pairs_count'] +=1                     # Pairs count
-                dicExchanges[ep.exchange_id]['volume'] += ep.volume_usd             # Volume
-                # -----/
-
-
-                # ----- Funding Rate data
-                data_row = ()
-                data_row = (f'{dateNow}',
-                            f'{timeNow}',
-                               fundingCycle,
-                            f'{ep.exchange_id}',
-                            f'{ep.symbol}',
-                            f'{ep.base}',
-                            f'{ep.target}',
-                            ep.funding_rate,
-                            ep.open_interest,
-                            ep.volume_usd,
-                            ep.spread
-                            )
-                data.append(data_row)
-                # -----/
-
-            self.logger.add('Data upload', f'Object Funding Rate Data processing into rows', 'Success',
-                            f'All data converted into rows')
-        except Exception as e:
-            error_message = str(e)[-255:]
-            self.logger.add('Data upload', f'Object Funding Rate Data processing into rows', 'Error',
-                            f'{error_message}')
-
-        try:
-            connection.insert_to_table(table_name,fr_columns,data)
-
-            self.logger.add('Data upload', f'Upload data to {table_name} table', 'Success',
-                            f'{len(data)} rows uploaded')
-        except PsycopgError as e:
-            # Specific database-related error
-            error_message = str(e)[-255:]
-            self.logger.add('Data upload', f'Upload data to {table_name} table', 'Error', error_message)
-            self.logger.exit_code_run_due_to_error(error_message)
-        except Exception as e:
-            # Other unexpected exceptions
-            error_message = str(e)[-255:]
-            self.logger.add('Data upload', f'Upload data to {table_name} table', 'Error', error_message)
-            self.logger.exit_code_run_due_to_error(error_message)
-
-        connection.add_to_action_log(table_name,self.action,len(data),
-                                     f'Data fetched from {len(dicExchanges)} exchanges',customTime=timeNow)
-
-        # ----------------------------------------/
+                    stillActive.append(f'{exchange_symbol}')
+        # -----/
 
         # ---------------------------------------- ACTIVE PAIRS
         try:
             data = []  # Reset
             for ex_pair, ep in dicToActivePairs.items():
                 data_row = ()
-                data_row = (f'{dateNow}',
+                data_row = (f'{self.dateNow}',
+                            f'{self.dateNow}',
                             f'{ep.exchange_id}',
                             f'{ep.base}',
                             f'{ex_pair}'
@@ -206,6 +110,166 @@ class Upload:
             error_message = str(e)[-255:]
             self.logger.add('Data upload', f'Upload data to active_pairs table', 'Error', error_message)
         # -----/
+        # ----------------------------------------/
+
+        # ---------------------------------------- UPDATE last_active date for the rest
+        try:
+            if len(stillActive) > 0:
+                connection.update_column_by_list_of_values(table_name=table_name,column='utc_last_active', newValue=f'{self.dateNow}',
+                                              where_col='exchange_id_symbol',params=(*stillActive,))
+
+            self.logger.add('Data upload', f'Update last_active date for each pair', 'Success',
+                            f'{len(stillActive)} pairs updated')
+
+        except Exception as e:
+            error_message = str(e)[-255:]
+            self.logger.add('Data upload', f'Update last_active date for each pair', 'Error', error_message)
+
+        # ----------------------------------------/
+
+        # ---------------------------------------- DELETE pairs that have not been above our threshold for over a week
+        try:
+            connection.delete_records(table_name=table_name,
+                                  where_clause_sql="utc_last_active < NOW() - INTERVAL '%s days'",params=(7,))
+
+            self.logger.add('Data upload', f'Delete old pairs from active_pairs', 'Success', 'Deleted')
+        except Exception as e:
+            error_message = str(e)[-255:]
+            self.logger.add('Data upload', f'Delete old pairs from active_pairs', 'Error', error_message)
+        # ----------------------------------------/
+
+
+        connection.close_connection()
+
+    def funding_rates_2h(self):
+
+        # ---------------------------------------- SETUP
+        # ----- Connect to Database and Get Table info
+        table_name = 'funding_rates_2h'
+
+        try:
+            connection = Connection(remote_server=self.remote_server)
+            self.logger.add('Data upload', 'Establishing connection to database', 'Success', 'Connected')
+        except PsycopgError as e:
+            # Specific database-related error
+            error_message = str(e)[-255:]
+            self.logger.add('Data upload', 'Establishing connection to database', 'Error', error_message)
+            self.logger.exit_code_run_due_to_error(error_message)
+        except Exception as e:
+            # Other unexpected exceptions
+            error_message = str(e)[-255:]
+            self.logger.add('Data upload', 'Establishing connection to database', 'Error', error_message)
+            self.logger.exit_code_run_due_to_error(error_message)
+
+        dicTableInfo = connection.get_table_info(table_name)
+        # -----/
+
+        # ----- Check Time difference between last upload and now
+        try:
+            fundingCycle = self.get_funding_cycle(refTime=datetime.now(timezone.utc).time(),is2hourCycle=True)
+
+            lastUpdDate = dicTableInfo['upload_last_date']
+            lastUpdTime = dicTableInfo['upload_last_time']
+            if lastUpdDate is not None:
+                lastFundingCycle = self.get_funding_cycle(refTime=lastUpdTime, is2hourCycle=True)
+            # -----/
+
+            self.logger.add('Data upload', 'Calculating funding Cycle', 'Success', 'Calculated')
+        except Exception as e:
+            error_message = str(e)[-255:]
+            self.logger.add('Data upload', 'Calculating funding Cycle', 'Error', f'{error_message}')
+            self.logger.exit_code_run_due_to_error(f'{error_message}')
+
+
+        # ----- In case we have data for the same funding cycle, delete the previous data and re-upload
+        try:
+            if lastUpdDate is not None and fundingCycle == lastFundingCycle and lastUpdDate == self.dateNow:
+                connection.delete_date_time_from_table(table_name=table_name,dateCol='utc_date',timeCol='utc_time',params=(lastUpdDate,lastUpdTime))
+                self.logger.add('Data upload', 'Delete previous data for the day and Cycle', 'Success', 'Deleted')
+        except Exception as e:
+            error_message = str(e)[-255:]
+            self.logger.add('Data upload', 'Delete previous data for the day and Cycle', 'Error', f'{error_message}')
+            self.logger.exit_code_run_due_to_error(f'{error_message}')
+        # -----/
+
+        # ----- Get column Names
+        fr_columns = list(self.tables_info.funding_rates().keys())      # Funding Rate columns
+        ul_columns = list(self.tables_info.update_log().keys())         # Upload log columns
+        # -----/
+        # ----------------------------------------/
+
+
+        # ---------------------------------------- FUNDING RATE DATA
+        try:
+            dicExchanges = {}           # Key: exchange_id, Value: dic with currency- and pairs counts
+            data = []
+            for ep in self.gecko.exchange_pairs.values():
+
+                # ----- Either our FR + Vol meets criteria this Cycle, or it met criteria in the past (is in active_pairs)
+                exchange_symbol = f'{ep.exchange_id}_{ep.symbol}'
+                if exchange_symbol in self.dicActivePairs:
+
+                    # ----- Update log data (for each exchange)
+                    if ep.exchange_id not in dicExchanges:
+                        dicExchanges[ep.exchange_id] = {}
+                        dicExchanges[ep.exchange_id]['currencies_count'] = 0
+                        dicExchanges[ep.exchange_id]['pairs_count'] = 0
+                        dicExchanges[ep.exchange_id]['volume'] = 0
+                        ul_data_row = ()
+                        currencies = []  # Currency list for the active exchange
+
+                    if ep.base not in currencies:
+                        dicExchanges[ep.exchange_id]['currencies_count'] +=1            # Currencies count
+                        currencies.append(ep.base)
+                    dicExchanges[ep.exchange_id]['pairs_count'] +=1                     # Pairs count
+                    dicExchanges[ep.exchange_id]['volume'] += ep.volume_usd             # Volume
+                    # -----/
+
+
+                    # ----- Funding Rate data
+                    data_row = ()
+                    data_row = (f'{self.dateNow}',
+                                f'{self.timeNow}',
+                                   fundingCycle,
+                                f'{ep.exchange_id}',
+                                f'{ep.symbol}',
+                                f'{ep.base}',
+                                f'{ep.target}',
+                                ep.funding_rate,
+                                ep.open_interest,
+                                ep.volume_usd,
+                                ep.spread
+                                )
+                    data.append(data_row)
+                    # -----/
+
+            self.logger.add('Data upload', f'Object Funding Rate Data processing into rows', 'Success',
+                            f'All data converted into rows')
+        except Exception as e:
+            error_message = str(e)[-255:]
+            self.logger.add('Data upload', f'Object Funding Rate Data processing into rows', 'Error',
+                            f'{error_message}')
+
+        try:
+            connection.insert_to_table(table_name,fr_columns,data)
+
+            self.logger.add('Data upload', f'Upload data to {table_name} table', 'Success',
+                            f'{len(data)} rows uploaded')
+        except PsycopgError as e:
+            # Specific database-related error
+            error_message = str(e)[-255:]
+            self.logger.add('Data upload', f'Upload data to {table_name} table', 'Error', error_message)
+            self.logger.exit_code_run_due_to_error(error_message)
+        except Exception as e:
+            # Other unexpected exceptions
+            error_message = str(e)[-255:]
+            self.logger.add('Data upload', f'Upload data to {table_name} table', 'Error', error_message)
+            self.logger.exit_code_run_due_to_error(error_message)
+
+        connection.add_to_action_log(table_name,self.action,len(data),
+                                     f'Data fetched from {len(dicExchanges)} exchanges',customTime=self.timeNow)
+
+        # ----------------------------------------/
 
 
         # ----------------------------------------/
@@ -217,8 +281,8 @@ class Upload:
             for exchange_id, dicData in dicExchanges.items():
 
                 data_row = ()
-                data_row = (f'{dateNow}',
-                            f'{timeNow}',
+                data_row = (f'{self.dateNow}',
+                            f'{self.timeNow}',
                                fundingCycle,
                             f'{exchange_id}',
                             dicData['currencies_count'],
@@ -252,7 +316,7 @@ class Upload:
             self.logger.add('Data upload', f'Upload data to update_log table', 'Error', error_message)
             self.logger.exit_code_run_due_to_error(error_message)
 
-        connection.add_to_action_log('update_log',self.action,len(data),'-',customTime=timeNow)
+        connection.add_to_action_log('update_log',self.action,len(data),'-',customTime=self.timeNow)
         # -----/
 
         connection.close_connection()
@@ -270,6 +334,12 @@ class Upload:
             fundingCycle = ((refTime.hour - 20) % 24) // 8 + 1
 
         return fundingCycle
+
+
+    def convert_2h_to_8h_data(self):
+        pass
+
+
 
     def calc_table(self):
         table_name = 'calc_table'
